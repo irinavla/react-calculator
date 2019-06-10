5a28ea0f0ed2c2238dcf9a986e414ff4d78cb02b
irinavla <irinav0402@gmail.com>
2019-06-10 20:59:40 +0300

    add 0 and multiply button

--- src/components/containers/Calculator.js
+++ src/components/containers/Calculator.js
@@ -8,31 +8,6 @@ import * as actions from '../../actions/actions';
  
 class Calculator extends Component {
     
-// performOperation(nextOperator) {    
-//     const { value, displayValue, operator } = this.props;
-//     const inputValue = parseFloat(displayValue);
-    
-//     if (value == null) {
-//       this.setState({
-//         value: inputValue
-//       })
-//     } else if (operator) {
-//       const currentValue = value || 0;
-//       const newValue = CalculatorOperations[operator](currentValue, inputValue);
-      
-//       this.setState({
-//         value: newValue,
-//         displayValue: String(newValue)
-//       })
-//     }
-    
-//     this.setState({
-//       waitingForOperand: true,
-//       operator: nextOperator
-//     })
-//   }
-  
-
 handleKeyDown = (ev) => {
     console.log('props',  this.props);
 
@@ -77,24 +52,27 @@ componentWillUnmount() {
 
             <div className="keypad">
                 <div className="keys">
-                <CalculatorKey className="clear vh-center" onPress={() => this.props.clearAll()}>clear</CalculatorKey>
+                    <CalculatorKey className="block clear vh-center border-bottom" onPress={() => this.props.clearAll()}>clear</CalculatorKey>
 
                     <div className="numbers">
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(7)}>7</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(8)}>8</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(9)}>9</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(4)}>4</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(5)}>5</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(6)}>6</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(1)}>1</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(2)}>2</CalculatorKey>
-                        <CalculatorKey className="key vh-center" onPress={() => this.props.inputDigit(3)}>3</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(7)}>7</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(8)}>8</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(9)}>9</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(4)}>4</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(5)}>5</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(6)}>6</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(1)}>1</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(2)}>2</CalculatorKey>
+                        <CalculatorKey className="key number vh-center" onPress={() => this.props.inputDigit(3)}>3</CalculatorKey>
                     </div>  
+
+                    <CalculatorKey className="block number vh-center" onPress={() => this.props.inputDigit(0)}>0</CalculatorKey>
 
                 </div>
                 <div className="operators">
 
                     <CalculatorKey className="operator vh-center" onPress={() => this.props.performOperation('/')}>÷</CalculatorKey>
+                    <CalculatorKey className="operator vh-center" onPress={() => this.props.performOperation('*')}>&times;</CalculatorKey>
                     <CalculatorKey className="operator vh-center" onPress={() => this.props.performOperation('-')}>-</CalculatorKey>
                     <CalculatorKey className="operator vh-center" onPress={() => this.props.performOperation('+')}>+</CalculatorKey>
                     <CalculatorKey className="operator vh-center" onPress={() => this.props.performOperation('=')}>=</CalculatorKey>  
